4657821f39ee135d97f44e50a38522f8910cd5bb
ahmedr1zwan <m.ahmedrizwan1@gmail.com>
2025-02-17 18:47:26 -0500

    added animated home

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import Navbar from './components/Navbar.jsx'
 import Home from './components/Home.jsx'
 import Techno from './components/Techno.jsx'
 import ExperienceProjects from './components/ExperienceProjects.jsx'
+import { BackgroundGradientAnimation } from './components/background-gradient-animation.jsx'
 
 
 function App() {
 
   return (
     <>
+  <BackgroundGradientAnimation />
+  <div className="relative z-10">
     <Navbar />
     <Home />
     <Techno />
     <ExperienceProjects />
-    </>
+  </div>
+  </>
+
   )
 }
 
